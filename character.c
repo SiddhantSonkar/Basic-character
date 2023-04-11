a278c5fa1d69c9e0ea139d344c362b0e9bdb477e
SiddhantSonkar <124968287+SiddhantSonkar@users.noreply.github.com>
2023-04-11 09:47:48 +0530

    Add files via upload

---
+++ character.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch[10];
+    printf("Enter the character:");
+    scanf("%s",&ch);
+    printf("The character is: %s",ch);
+    
+    
+}
